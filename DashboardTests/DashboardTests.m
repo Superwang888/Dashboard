1954e21801137e95bd686c1b286d8150c4d71d81
王灵博 <18301397045@163.com>
2019-06-10 15:46:39 +0800

    all

---
+++ DashboardTests/DashboardTests.m
@@ -0,0 +1,37 @@
+//
+//  DashboardTests.m
+//  DashboardTests
+//
+//  Created by 王灵博 on 2019/6/6.
+//  Copyright © 2019 王灵博. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DashboardTests : XCTestCase
+
+@end
+
+@implementation DashboardTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
